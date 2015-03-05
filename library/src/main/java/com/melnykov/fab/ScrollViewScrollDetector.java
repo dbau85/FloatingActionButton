49906f8d50ace419685c3b714756f9156bfb9e99
Daniel Bauer <dbau@kupferwerk.net>
2015-03-05 09:20:45 +0100

    test

--- library/src/main/java/com/melnykov/fab/ScrollViewScrollDetector.java
+++ library/src/main/java/com/melnykov/fab/ScrollViewScrollDetector.java
@@ -13,13 +13,13 @@ abstract class ScrollViewScrollDetector implements ObservableScrollView.OnScroll
     @Override
     public void onScrollChanged(ScrollView who, int l, int t, int oldl, int oldt) {
         boolean isSignificantDelta = Math.abs(t - mLastScrollY) > mScrollThreshold;
-        if (isSignificantDelta) {
-            if (t > mLastScrollY) {
-                onScrollUp();
-            } else {
-                onScrollDown();
-            }
-        }
+//        if (isSignificantDelta) {
+//            if (t > mLastScrollY) {
+//                onScrollUp();
+//            } else {
+//                onScrollDown();
+//            }
+//        }
         mLastScrollY = t;
     }
 
